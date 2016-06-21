158ac659b5a6632f1d7d9c001224b08c2c75bd85
Michael Dijamco <mdd233@cornell.edu>
2016-06-20 22:44:22 -0400

    feat: declare and populate education object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,3 +13,74 @@ var bio = {
     , skills        : ['HTML', 'CSS', 'Javascript']
     , biopic        : 'xxx.jpg'
 };
+
+var education = {
+      school        : [
+          {
+                name        : 'Cornell University'
+              , location    : 'Ithaca, NY'
+              , degree      : 'Bachelor of Science in Engineering'
+              , majors      : ['Mechanical Engineering']
+              , dates       : '2011-2016'
+              , url         : 'http://www.cornell.edu/'
+          }
+    ]
+    , onlineCourses : [
+        {
+                title       : 'Intro to HTML and CSS (ud304)'
+              , school      : 'Udacity'
+              , date        : '2016'
+              , url         : 'https://www.udacity.com/course/intro-to-html-and-css--ud304'
+        },{
+                title       : 'Responsive Web Design Fundamentals (ud893)'
+              , school      : 'Udacity'
+              , date        : '2016'
+              , url         : 'https://www.udacity.com/course/responsive-web-design-fundamentals--ud893'
+        },{
+                title       : 'Responsive Images (ud882)'
+              , school      : 'Udacity'
+              , date        : '2016'
+              , url         : 'https://www.udacity.com/course/responsive-images--ud882'
+        },{
+                title       : 'How to Use Git and GitHub (ud775)'
+              , school      : 'Udacity'
+              , date        : '2016'
+              , url         : 'https://www.udacity.com/course/how-to-use-git-and-github--ud775'
+        },{
+                title       : 'Javascript Basics (ud804)'
+              , school      : 'Udacity'
+              , date        : '2016'
+              , url         : 'https://www.udacity.com/course/javascript-basics--ud804'
+        },{
+                title       : 'Intro to jQuery (ud245)'
+              , school      : 'Udacity'
+              , date        : '2016'
+              , url         : 'https://www.udacity.com/course/intro-to-jquery--ud245'
+        },{
+                title       : 'Javascript: Understanding the Weird Parts'
+              , school      : 'Udemy'
+              , date        : '2015'
+              , url         : 'https://www.udemy.com/understand-javascript/'
+        },{
+                title       : 'Learn and Understand NodeJS'
+              , school      : 'Udemy'
+              , date        : '2015'
+              , url         : 'https://www.udemy.com/understand-nodejs/'
+        },{
+                title       : 'Learn and Understand AngularJS'
+              , school      : 'Udemy'
+              , date        : '2015'
+              , url         : 'https://www.udemy.com/learn-angularjs/'
+        },{
+                title       : 'Learn and Understand AngularJS'
+              , school      : 'Udemy'
+              , date        : '2015'
+              , url         : 'https://www.udemy.com/learn-angularjs/'
+        },{
+                title       : 'CS 1110: Introduction to Computing Using Python (Spring 2015)'
+              , school      : 'Cornell University'
+              , date        : '2015'
+              , url         : 'http://www.cs.cornell.edu/courses/cs1110/2015sp/index.php'
+        }
+    ]
+};
