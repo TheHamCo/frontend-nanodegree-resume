63caa05ba8705f6b2141c3701d3af1214cb5c6da
Michael Dijamco <mdd233@cornell.edu>
2016-06-20 22:27:39 -0400

    feat: Create bio object
    Only data, no display function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/*DATA*/
+var bio = {
+      name          : 'Michael Dijamco'
+    , role          : 'Front-End Web Developer'
+    , contacts      : {
+          mobile    : 'xxx-xxx-xxxx'
+        , email     : 'mdd233@cornell.edu'
+        , github    : 'TheHamCo'
+        , twitter   : 'none'
+        , location  : 'Jackson, NJ'
+    }
+    , welcomeMessage: 'I am a recent graduate looking for a developer job in NYC.  See my work:'
+    , skills        : ['HTML', 'CSS', 'Javascript']
+    , biopic        : 'xxx.jpg'
+};
